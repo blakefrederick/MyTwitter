7dbf3b94941a9743501f1bfffcaafae7b16034b5
Blake Frederick <bf@six12creative.com>
2024-11-07 21:14:15 -0800

    Initial Commit

---
+++ MyTwitter/MyTwitterApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyTwitterApp.swift
+//  MyTwitter
+//
+//  Created by Blake Frederick on 2024-11-07.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyTwitterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
