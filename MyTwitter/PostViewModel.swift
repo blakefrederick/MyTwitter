e1677aee8e2d813ffa0f1a5426d187e1761fac46
Blake Frederick <bf@six12creative.com>
2024-11-10 20:11:42 -0800

    100x more efficient post fetching

--- MyTwitter/PostViewModel.swift
+++ MyTwitter/PostViewModel.swift
@@ -11,15 +11,23 @@ class PostViewModel: ObservableObject {
     }
     
     func fetchPosts() {
-        db.collection("posts").getDocuments { snapshot, error in
-            if let error = error {
-                print("Error fetching posts: \(error)")
-                return
+        let randomValue = Double.random(in: 0...1)
+        
+        db.collection("posts")
+            .whereField("randomValue", isGreaterThanOrEqualTo: randomValue)
+            .limit(to: 55)
+            .getDocuments { snapshot, error in
+                if let error = error {
+                    print("Error fetching posts: \(error)")
+                    return
+                }
+                
+                guard let documents = snapshot?.documents else { return }
+                let posts = documents.compactMap { try? $0.data(as: Post.self) }
+                
+                DispatchQueue.main.async {
+                    self.posts = posts
+                }
             }
-            let allPosts = (snapshot?.documents ?? []).compactMap { document -> Post? in
-                try? document.data(as: Post.self)
-            }
-            self.posts = Array(allPosts.shuffled().prefix(55))
-        }
     }
 }
